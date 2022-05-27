6e914b1b628f3a5ed04ea9655c9fa8b8e01282ec
Miguel Soares <98955643+MiguelSoaresDev@users.noreply.github.com>
2022-05-27 08:56:08 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,85 @@
+const celulas = document.querySelectorAll(".celula");
+let checarTurno = true;
+
+const JOGADOR_X = "X";
+const JOGADOR_O = "O";
+
+const COMBINACOES = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+document.addEventListener("click", (event) => {
+    if (event.target.matches(".celula")) {
+        jogar(event.target.id);
+    }
+})
+function jogar(id) {
+    const celula = document.getElementById(id);
+    turno = checarTurno ? JOGADOR_X : JOGADOR_O;
+    celula.textContent = turno;
+    celula.classList.add(turno);
+    checarVencedor(turno);
+}
+function checarVencedor(turno) {
+    const vencedor = COMBINACOES.some((comb) => {
+        return comb.every((index) => {
+            return celulas[index].classList.contains(turno);
+        })
+    });
+
+    if (vencedor) {
+        encerrajogo(turno);
+    } else if (checarEmpate()) {
+        encerrajogo();
+    } else {
+        checarTurno = !checarTurno;
+    }
+}
+function checarEmpate() {
+    let x = 0;
+    let o = 0;
+
+    for (index in celulas) {
+        if (!isNaN(index)) {
+
+            if (celulas[index].classList.contains(JOGADOR_X)) {
+                x++;
+            }
+
+            if (celulas[index].classList.contains(JOGADOR_O)) {
+                o++;
+            }
+        }
+    }
+    return x + o == 9 ? true : false;
+}
+function encerrajogo(vencedor = null) {
+    const telsEscura = document.getElementById("tela-escura");
+    const h2 = document.createElement("h2");
+    const h3 = document.createElement("h3");
+    let mensagem = null;
+
+    telsEscura.style.display = "block"
+    telsEscura.appendChild(h2)
+    telsEscura.appendChild(h3);
+
+
+    if (vencedor) {
+        h2.innerHTML = `o player <span>${vencedor}</span> venceu`;
+    } else {
+        h2.innerHTML = "empatou";
+    }
+
+    let contador = 3;
+    setInterval(() => {
+        h3.innerHTML = `reiniciando em ${contador--}`;
+    }, 1000);
+
+    setTimeout(() => location.reload(), 4000);
+}
